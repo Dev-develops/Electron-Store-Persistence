6a1141e263e0c57a51c7a0cc38c947e5b28c00e3
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-09 12:54:37 +0530

    Seemingly

--- preload.js
+++ preload.js
@@ -1,6 +1,33 @@
 window.addEventListener('DOMContentLoaded', () => {
-    var sample = document.getElementById('sample');
     var submit = document.getElementById('submit');
 
     const Store = require('electron-store');
+
+    // json schema implementation.
+    const userSchema = {
+        userid: {
+            type: 'number'
+        },
+        name: {
+            type: 'string'
+        },
+        // zone: {
+        //     type: 'string'
+        // }
+    }
+    const store = new Store({
+        schema: userSchema,
+        encryptionKey: '1234567890-poiuyhjklmnb-vcxzhgfdytre-qwaszx-1234567890'
+    });
+
+    submit.addEventListener('click', () => {
+        var sample = document.getElementById('sample').value;
+
+        store.set('userid', 11111);
+        store.set('name', sample);
+
+        console.log(store.get('userid'));
+        console.log(store.get('name'));
+        console.log(sample);
+    })
 })
